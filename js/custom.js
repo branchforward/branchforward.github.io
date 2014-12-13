06d9d74cd2b3b1634b69455f3a8e1a4ba3112042
vivekrs <vivekrs.rsv@gmail.com>
2014-12-12 22:55:07 -0600

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+(function ($) {
+	$("nav a[href!='#']").on('click', function(e) {
+	   // store hash
+	   var target = this.hash;
+
+	   // prevent default anchor click behavior
+	   e.preventDefault();
+
+	   // animate
+	   $('html, body').animate({
+		   scrollTop: $(this.hash).offset().top
+		 }, 1000, function(){
+
+		   // when done, add hash to url
+		   // (default click behaviour)
+		   window.location.hash = target;
+		 });
+	});
+
+})(jQuery);
